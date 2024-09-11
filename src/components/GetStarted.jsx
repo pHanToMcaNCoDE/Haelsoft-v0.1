35a366a0bd5e099b24113b124f45f656a429953f
phantomcancode <vodumeh@gmail.com>
2024-09-11 10:09:17 +0100

    reduced spacing on hero section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -12,8 +12,8 @@ const GetStarted = () => {
           </div>
           <div className='w-full flex justify-center items-center xl:w-[50%]'>
               <div className='bg-[#FBFBFB] flex flex-col justify-center items-center rounded w-full relative lg:w-[626px] h-[660px] gap-4 p-[10px] md:p-[25px] lg:p-[50px]'>
-                  <div className='bg-main absolute top-3 left-0 w-full lg:w-[625px] rounded-lg h-[6px]'></div>
-                  <h1 className='leading-[43px] text-main font-semibold text-[1.75rem] text-center xl:text-left w-full'>Book a Consultation for Free</h1>
+                  <div className='bg-[#F36400] absolute top-3 left-0 w-full lg:w-[625px] rounded-lg h-[6px]'></div>
+                  <h1 className='leading-[43px] text-[#F36400] font-semibold text-[1.75rem] text-center xl:text-left w-full'>Book a Consultation for Free</h1>
                   <p className='text-[#655D59] font-normal text-[1rem] leading-[24px] w-full xl:w-[297px] text-center xl:text-left'>
                       Kindly fill in the form to get started.
                   </p>
@@ -23,9 +23,9 @@ const GetStarted = () => {
                           <input className='w-full lg:w-[482px] h-[48px] rounded py-[15px] px-[20px] gap-[10px] bg-white placeholder:text-[#655D5980] placeholder:text-[.75rem] font-normal placeholder:font-normal placeholder:leading-[18px] text-base outline-none border-none' type="text" name="" id="" placeholder='Email Address' />
                           <input className='w-full lg:w-[482px] h-[48px] rounded py-[15px] px-[20px] gap-[10px] bg-white placeholder:text-[#655D5980] placeholder:text-[.75rem] font-normal placeholder:font-normal placeholder:leading-[18px] text-base outline-none border-none' type="text" name="" id="" placeholder='Phone Number' />
                       </div>
-                      <button className='bg-main gap-[10px] p-[10px] w-full lg:w-[298px] rounded flex justify-center items-center h-[58px] text-white' type="submit">Request Consultation</button>
+                      <button className='bg-[#F36400] gap-[10px] p-[10px] w-full lg:w-[298px] rounded flex justify-center items-center h-[58px] text-white' type="submit">Request Consultation</button>
                   </div>
-                  <div className='bg-main absolute bottom-3 left-0 w-full lg:w-[625px] rounded-lg h-[6px]'></div>
+                  <div className='bg-[#F36400] absolute bottom-3 left-0 w-full lg:w-[625px] rounded-lg h-[6px]'></div>
               </div>
           </div>
       </div>
