87db0f2d13e2078c16492bc2496efd3c2f022d85
phantomcancode <vodumeh@gmail.com>
2024-09-11 10:44:48 +0100

    changes made on the navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                   <h1 className='text-[3.5rem] lg:text-[4.313rem] text-left text-[#FFCF00] font-semibold leading-[55px] lg:leading-[75px]'>Innovate. Lead. Succeed.</h1>
                   <p className='text-[1.25rem] leading-6 text-white text-left'>Jumpstart Your Tech Career and Lead the Future</p>
                 </div>
-                <a href="#get-started" className="py-[15px] px-[24px] border border-[#F36400] bg-[#F36400] text-white font-normal text-[1.125rem] leading-10 h-[45px] w-[161px] flex justify-center items-center gap-2 rounded">Get Started</a>
+                <a href="#get-started" className="py-[15px] px-[24px] border border-[#F36400] bg-[#F36400] text-white font-normal text-[1.125rem] leading-10 h-[48px] w-[181px] flex justify-center items-center gap-2 rounded">Get Started</a>
             </div>
         </div>
       </div>
